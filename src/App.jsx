449f2665f89d150ed1bfbdf495fbc55dc8b37e16
Erik Daniel <erik@Eriks-MacBook-Air-5.local>
2026-09-23 20:23:02 -0700

    basic sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
 import { supabase } from './services/supabase'
+import './AppStyle.css';
 
 function App() {
   return (
     <div className="container mt-5">
       <h1>Study Buddy</h1>
       <p>Find the right study partner.</p>
+      <div className='sign-container'>
+        <a href="" className='sign-btn'>Create an account</a>
+        <a href="" className='sign-btn'>Sign in</a>
+      </div>
     </div>
   )
 }
+
 
 export default App
