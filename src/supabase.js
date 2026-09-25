619b2485a0d18ca01745773bd5e942b89ffe62c2
Artin-Etaati <161558173+Artin-Etaati@users.noreply.github.com>
2026-09-25 13:04:05 -0700

    updated features/authentication practice

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.local.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.local.VITE_SUPABASE_PUBLISHABLE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
